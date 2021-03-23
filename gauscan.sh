832f41f9fd25255eb82d091aaabd34b89e7df079
nullenc0de <pseekamp@gmail.com>
2021-03-23 15:44:23 -0400

    Update gauscan.sh

--- gauscan.sh
+++ gauscan.sh
@@ -15,16 +15,38 @@ kill (){
 recon (){
 banner
 
+#Find gau endpoints and validate 200 code
 echo $1 |gau -subs |qsreplace -a| ffuf -w - -u FUZZ -t 400 -mc 200 -o 200url.csv -of csv
+
+#extract ffuf endpoints 
 cat 200url.csv |cut -d , -f3|qsreplace -a > endpoint.txt
+
+#extract ffuf endpoints 
 cat 200url.csv |cut -d , -f2|qsreplace -a >> endpoint.txt
+
+#find interesting params
 cat endpoint.txt |grep = |egrep -v ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|pdf|svg|txt|js)" |qsreplace -a |gf interestingparams |while read url; do injectx.py -u $url -vn ; done |tee scan.raw
+
+#convert the binary output to text
 strings scan.raw |grep "Exploit Command" |sort -u |cut -c 9- > gauscan.txt
+
+#extract sqlmap findings to put in file append output dir
+cat gauscan.txt |grep sqlmap |cut -c 18- |sed 's/$/ --output-dir=.\//' > ./sql_exploit.sh
+
+#remove sqlmap from results
+sed -i '/sqlmap/d' gauscan.txt
+
+#upload findings to slack without sqlmap
 slackcat --channel bugbounty ./gauscan.txt
-rm gauscan.txt
-rm scan.raw
-rm 200url.csv
-rm endpoint.txt
+
+#validate sqlmap finds
+bash ./sql_exploit.sh
+
+#pull out sqlmap findings
+find ./ -name 'target.txt' -exec cat {} \; |sort -u > ./possible_sqli.txt
+
+#upload sqlmap findings to slack
+slackcat --channel bugbounty ./possible_sqli.txt
 
 }
 
