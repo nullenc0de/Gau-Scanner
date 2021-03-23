c82c87ec04553ed931861654f2daf972b8b070eb
nullenc0de <pseekamp@gmail.com>
2021-03-23 13:27:35 -0400

    Update gauscan.sh

--- gauscan.sh
+++ gauscan.sh
@@ -17,6 +17,7 @@ banner
 
 echo $1 |gau -subs |qsreplace -a| ffuf -w - -u FUZZ -t 400 -mc 200 -o 200url.csv -of csv
 cat 200url.csv |cut -d , -f3|qsreplace -a > endpoint.txt
+cat 200url.csv |cut -d , -f2|qsreplace -a >> endpoint.txt
 cat endpoint.txt |grep = |egrep -v ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|pdf|svg|txt|js)" |qsreplace -a |gf interestingparams |while read url; do injectx.py -u $url -vn ; done |tee scan.raw
 strings scan.raw |grep "Exploit Command" |sort -u |cut -c 9- > gauscan.txt
 slackcat --channel bugbounty ./gauscan.txt
